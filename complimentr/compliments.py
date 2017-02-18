bc08fab6d09d36981d7972ceccd3f36957b2e9d3
Chandni Patel <chandnir.patel@gmail.com>
2017-02-18 11:43:53 -0800

    Add tasks to connect with twilio

--- complimentr/compliments.py
+++ complimentr/compliments.py
@@ -1,3 +1,9 @@
+
+# Address of Redis instance, our Celery broker
+BROKER_URL = 'redis://localhost:6379/0'
+BROKER_POOL_LIMIT = 8
+
+
 def runComplimentrApp ():
-  print("running app from compliment file")
+  print("to be implemented")
   return
