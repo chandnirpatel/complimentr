6037f69dafb2acc2ef764fc986ca01a7454f0b17
Chandni Patel <chandnir.patel@gmail.com>
2017-02-18 14:22:20 -0800

    Send a text message when you run complimentr

--- complimentr/compliments.py
+++ complimentr/compliments.py
@@ -1,9 +1,6 @@
-
-# Address of Redis instance, our Celery broker
-BROKER_URL = 'redis://localhost:6379/0'
-BROKER_POOL_LIMIT = 8
-
+from complimentr.messageSender import sendCompliment
 
 def runComplimentrApp ():
-  print("to be implemented")
+  print("compliment will be sent soon")
+  sendCompliment()
   return
