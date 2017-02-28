43cf7bab433957105fc22185453dbbe531144207
Chandni Patel <chandnir.patel@gmail.com>
2017-02-27 22:06:54 -0800

    Send compliment every 5 seconds until 10 are sent

--- complimentr/compliments.py
+++ complimentr/compliments.py
@@ -1,7 +1,12 @@
 from complimentr.messageSender import sendCompliment
+import time
 
 def runComplimentrApp ():
   print("Welcome to complimentr! \n You will need set up a Twilio Account before using this app")
-  sendCompliment()
+  compliment_count = 0
+  while (compliment_count <= 10):
+      time.sleep(5)
+      sendCompliment()
+
   print("Compliment sent!")
   return
